adc1f9b6500cc029b4e2e89db592b11f0f36ca57
GSTRprojects <GSTRprojects@gmail.com>
2018-06-07 12:36:26 +0200

    Initial commit

---
+++ assets/php/main.php
@@ -0,0 +1,81 @@
+<div id="home" class="container">
+  <div class="row background-top">
+    <div class="col-2 col-s-1"></div>
+    <div class="col-8 col-s-10">
+      <div class="container logo-container">
+        <img class="img-responsive" src="assets/img/logo.png" alt="Logo"/>
+      </div>
+      <div class="container">
+        <p class="over">Over&nbsp;</p>
+        <h1>5000 AFN</h1>
+        <div class="row jackpot">
+          <div class="col-s-1 col-1"></div>
+          <div class="col-s-11 col-11">
+            <p class="in">in</p>
+            <div class="img-container">
+              <img class="img-responsive" src="assets/img/jackpot.png" alt="Jackpot"/>
+            </div>
+            <div class="container prize">
+              <p class="usd">1.000.000 VND!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container bet">
+        <div class="row">
+          <div class="col-s-3 col-3">
+            <div>GAMES PLAYED</div>
+            <img class="img-responsive" src="assets/img/counter.png" alt="counter"/>
+          </div>
+          <div class="col-s-9 col-9">
+            <span class="win">Bet <span class="yellow">0.001</span> AFN HIT 6 <b>and WIN</b> <span class="yellow"><b>2000 AFN!!!!</b></span></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row background-bottom">
+    <div class="col-2 col-s-1"></div>
+    <div class="col-8 col-s-10">
+      <div class="container">
+        <div class="rules">
+          <p>LOREM IPSUM</p>
+        </div>
+        <div class="row content">
+          <div class="col-7 col-s-7">
+            <table>
+              <tr>
+                <td>
+                  <img class="img-responsive" src="assets/img/circle1.png" alt="circle1"/>
+                </td>
+                <td><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p></td>
+              </tr>
+              <tr>
+                <td>
+                  <img class="img-responsive" src="assets/img/circle2.png" alt="circle2"/>
+                </td>
+                <td><p>Iaculis erat at, commodo leo. Integer pulvinar </p></td>
+              </tr>
+              <tr>
+                <td>
+                  <img class="img-responsive" src="assets/img/circle3.png" alt="circle3"/>
+                </td>
+                <td><p>Sed eget rhoncus mi. Proin posuere, felis non pellentesque eleifend </p></td>
+              </tr>
+              <tr>
+                <td>
+                  <img class="img-responsive" src="assets/img/circle4.png" alt="circle4"/>
+                </td>
+                <td><p>Vestibulum sit amet nisi et nulla fermentum egestas </p></td>
+              </tr>
+            </table> 
+          </div>
+          <div class="col-5 col-s-5">		
+            <a href="#" class="btn">LOREM IPSUM ></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row"></div>
+</div>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon" />
+    <title>Light onepager - no Bootstrap</title>
+  </head>
+  <body>
+    <?php include ('assets/php/main.php'); ?>
+  </body>
+</html>
